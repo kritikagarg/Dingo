95b2d1a9b13eb81b5e0747ed275d09613b030c4a
Kritika Garg <kgarg001@odu.edu>
2021-10-15 16:06:37 -0400

    Add files via upload

---
+++ test/use_dingo.js
@@ -0,0 +1,8 @@
+(async function() {
+    //class Dingo {};
+    const dingo = new Dingo();
+    console.log(dingo.in_archive);
+    console.log(dingo.get_mementodatetime());
+    dingo.cloak();
+
+})();
